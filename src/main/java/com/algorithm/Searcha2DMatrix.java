1d1be5012e978b3b64bc59d15794b3273f4eeb1d
Zhanghj <550727632@qq.com>
2016-11-22 14:56:56 +0800

    行列命名错误

--- src/main/java/com/algorithm/Searcha2DMatrix.java
+++ src/main/java/com/algorithm/Searcha2DMatrix.java
@@ -13,15 +13,15 @@ public class Searcha2DMatrix {
 	public boolean searchMatrix(int[][] matrix, int target) {
 		if (matrix.length == 0) return false;
         boolean found = false;
-        int columns = matrix.length;
-        int rows = matrix[0].length;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
         int row = 0;
         int col = columns-1;
         while (row < rows && col >= 0) {
-			if (matrix[col][row] == target) {
+			if (matrix[row][col] == target) {
 				found = true;
 				break;
-			} else if (matrix[col][row] > target) {
+			} else if (matrix[row][col] > target) {
 				-- col;
 			} else {
 				++ row;
